ed6d791f7353592bd0986d9f353ab79cf0d24caf
theemadnes <alex.mattson@gmail.com>
2024-09-12 06:45:42 +0000

    calling works

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/joho/godotenv"
 )
@@ -59,10 +60,65 @@ func evaluateHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	callResponseChan := make(chan string) // Create a channel
+	errorChan := make(chan error)         // Channel for errors
+
+	// Attempt to call the downstream service
+	go func() {
+		fmt.Println("Calling downstream service")
+		resp, err := http.Get("https://golang-hello-word-603904278888.us-central1.run.app")
+		if err != nil {
+			// Handle error (e.g., log the error)
+			//fmt.Println("Error making GET request:", err)
+			errorChan <- err // Send the error to the channel
+			return           // Exit the goroutine if there's an error
+		}
+		defer resp.Body.Close()
+
+		// Read the response body
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Error reading response body:", err)
+			return
+		}
+
+		// Create a reader for the JSON response
+		reader := strings.NewReader(string(body))
+
+		// Decode the JSON response
+		var result map[string]interface{}
+		if err := json.NewDecoder(reader).Decode(&result); err != nil {
+			fmt.Println("Error decoding JSON:", err)
+			return
+		}
+		fmt.Println("Response body:", string(body))
+
+		fmt.Println("Message", result["message"])
+
+		// Update callResponse if the GET request is successful
+		callResponseChan <- result["message"].(string)
+	}()
+
 	// Respond with the evaluation result
-	response := map[string]interface{}{
-		"compressed": contentEncoding == "gzip",
-		"data":       data,
+	select {
+	case callResponse := <-callResponseChan:
+		// ... (Respond with the evaluation result) ...
+		response := map[string]interface{}{
+			"compressed": contentEncoding == "gzip",
+			"data":       data,
+			"call":       callResponse,
+		}
+		json.NewEncoder(w).Encode(response)
+	case err := <-errorChan:
+		// Handle the error from the downstream service
+		fmt.Println("Error from downstream service:", err)
+		//http.Error(w, "Error calling downstream service", http.StatusInternalServerError)
+		response := map[string]interface{}{
+			"compressed": contentEncoding == "gzip",
+			"data":       data,
+			"call":       "error",
+		}
+		json.NewEncoder(w).Encode(response)
 	}
-	json.NewEncoder(w).Encode(response)
+
 }
